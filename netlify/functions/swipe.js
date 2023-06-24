2d39168d1557cf7a4b111f0dab4f389b357faebb
jaime <garciadiazjaime@gmail.com>
2023-06-24 16:44:23 -0500

    feat: debug swipe lambda

--- netlify/functions/swipe.js
+++ netlify/functions/swipe.js
@@ -1,11 +1,10 @@
-const {
-  saveSwipe,
-} = require("../../support/dynamo");
+const { saveSwipe } = require("../../support/dynamo");
 
 exports.handler = async function (event, _context) {
+  console.log(JSON.stringify(event, null, 2));
   if (event.httpMethod === "POST") {
     const { post_id, user_uuid, swipe } = event.multiValueQueryStringParameters;
-
+    console.log({ post_id, user_uuid, swipe });
     if (!post_id?.length || !user_uuid?.length || !swipe?.length) {
       return {
         statusCode: 400,
